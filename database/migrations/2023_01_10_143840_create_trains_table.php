ccadf9827aea7f0780a42ba5d20eacbbc63a7db0
andreaa21 <andrea.zollini@proton.me>
2023-01-10 15:56:46 +0100

    add migration

---
+++ database/migrations/2023_01_10_143840_create_trains_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            /**
+             * Azienda
+             * Stazione di partenza
+             * Stazione di arrivo
+             * Orario di partenza
+             * Orario di arrivo
+             * Codice Treno
+             * Numero Carrozze
+             * In orario
+             * Cancellato
+             */
+            $table->string('azienda', 15);
+            $table->string('stazione_di_partenza', 30);
+            $table->string('stazione_di_arrivo', 30);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->string('codice_treno', 4);
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('is_on_time')->unsigned()->default(1);
+            $table->boolean('is_canceled')->unsigned()->default(0);
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
